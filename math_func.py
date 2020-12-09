4b3406ddf2b5cca6bc8ec16e44ad7b07414b676a
cwsaunders <csaunders5@liberty.edu>
2020-12-09 18:47:42 -0500

    new file and studentdb class

--- math_func.py
+++ math_func.py
@@ -1,6 +1,21 @@
+import json
+
 def boolean(a,b):
     return a == b
 
 def addition(a,b):
     return a+b
 
+# class for 39:29
+class StudentDB:
+    def __init__(self):
+        self.__data = None
+    
+    def connect(self,data_file):
+        with open(data_file) as json_file:
+            self.__data = json.load(json_file)
+    
+    def get_data(self,name):
+        for student in self.__data['students']:
+            if student['name'] == name:
+                return student
---
+++ test_math_func_2.py
@@ -0,0 +1,11 @@
+from math_func import StudentDB
+import pytest
+
+def test_scott_data():
+
+
+
+
+
+def test_mark_data():
+    
