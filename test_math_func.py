79e87d23421217b627a54c9ba0c7899e27eab681
cwsaunders <csaunders5@liberty.edu>
2020-12-09 16:43:07 -0500

    more tests

--- test_math_func.py
+++ test_math_func.py
@@ -2,9 +2,10 @@ import math_func
 
 def test_boolean():
     assert math_func.boolean(5,5) == True
-    assert math_func.boolean(5,4) == True
+    assert math_func.boolean(5,4) == False
 
 def test_addition():
     assert math_func.addition(4,1) == 5
-    assert math_func.addition(6,6) == 11
+    assert math_func.addition(6,6) == 12
+    assert math_func.addition(6,6) == 13
 
