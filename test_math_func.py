44e79036f6b45ee4941d6f81ddfd3533bfe1eaa9
cwsaunders <csaunders5@liberty.edu>
2020-12-09 17:15:45 -0500

    markers added

--- test_math_func.py
+++ test_math_func.py
@@ -1,9 +1,12 @@
 import math_func
+import pytest
 
+@pytest.mark.boolean # marker
 def test_boolean(): # test_ prefix required for tests
     assert math_func.boolean(5,5) == True
     assert math_func.boolean(5,4) == False
-
+    
+@pytest.mark.number # marker
 def test_addition():
     assert math_func.addition(4,1) == 5
     assert math_func.addition(6,6) == 12
