d1d482532da6c02c14d9a5ea271d0d41ebb2141a
cwsaunders <csaunders5@liberty.edu>
2020-12-10 15:01:30 -0500

    fixture added

--- test_math_func_2.py
+++ test_math_func_2.py
@@ -1,7 +1,7 @@
 from math_func import StudentDB
 import pytest
 
-@pytest.fixture
+@pytest.fixture(scope='module') # Only calls DB setup once, not at beginning of every test run
 def db():
     print('------------Setup------------')
     db = StudentDB()
@@ -12,13 +12,13 @@ def db():
 #     print('------------Teardown------------')
 #     db.close()
 
-def test_scott_data(db):
+def test_scott_data(db): # returned db argument from @pytest.fixture
     scott_data = db.get_data('Scott')
     assert scott_data['id'] == 1
     assert scott_data['name'] == 'Scott'
     assert scott_data['result'] == 'pass'
 
-def test_mark_data(db):
+def test_mark_data(db): # returned db argument from @pytest.fixture
     mark_data = db.get_data('Mark')
     assert mark_data['id'] == 2
     assert mark_data['name'] == 'Mark'
